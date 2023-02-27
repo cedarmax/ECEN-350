1b15d0b4b1522a786998167b74023ac78ad89f63
Cedar Maxwell <cedarmaxwell@mac.com>
2023-02-27 10:55:38 -0600

    working on lab 4

--- Lab04/main.c
+++ Lab04/main.c
@@ -3,10 +3,13 @@
 #include <stdio.h>
 
 extern long long int my_mul(long long int a, long long int b);
+extern long long int lab04b(long long int m);
 
 int main(void)
 {
-    long long int a = my_mul(3, 5);
-    printf("Result of my_mul(3, 5) = %lld\n", a);
+//    long long int a = my_mul(3, 5);
+//    printf("Result of my_mul(3, 5) = %lld\n", a);
+	long long int n = lab04b(5);
+	printf("Result of lab04b(5) = %lld\n", n);
     return 0;
 }
